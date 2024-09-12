7675926037f0365a2187813b9a01e20ba12ebd3b
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-09-12 11:53:07 -0700

    Create NAG-L05L01-VARS.sh

---
+++ NAG-L05L01-VARS.sh
@@ -0,0 +1,17 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
+# update nginx host for the specific lab
+
+#Remove default.conf file since this lab requires only default.bak
+sudo ssh nginx rm /etc/nginx/conf.d/default.conf
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/default.bak
+sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab05/mywebserver.bak
+sudo scp /tmp/mywebserver.bak                              nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/return_test.bak
+sudo scp /tmp/return_test.bak                              nginx:/etc/nginx/conf.d/

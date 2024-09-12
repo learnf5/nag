15111546e7b800a8ffcab11fd8917c768bd5f2d3
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-09-12 11:54:05 -0700

    Update NAG-L03L01-SRVSEL.sh

--- NAG-L03L01-SRVSEL.sh
+++ NAG-L03L01-SRVSEL.sh
@@ -4,7 +4,7 @@ PS4='+$(date +"%T.%3N"): '
 
 # update Student Workstation host for the specific lab
 # only if need a simpler /etc/hosts file do the following 2 steps
-# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab01/hosts
+# curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab01/hosts
 # sudo cp /tmp/hosts                                  /etc/hosts
 
 # update nginx host for the specific lab
@@ -12,8 +12,8 @@ PS4='+$(date +"%T.%3N"): '
 #Remove default.conf file since this lab requires only default.bak
 sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/default.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/default.bak
 sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nad32/main/lab03/mywebserver.bak
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/mywebserver.bak
 sudo scp /tmp/mywebserver.bak                              nginx:/etc/nginx/conf.d/

0ee60130470c0b48f3acdd72ac94f2f31cb4380c
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-03-06 10:21:53 -0800

    Update NAG-L03L01-SRVSEL.sh

--- NAG-L03L01-SRVSEL.sh
+++ NAG-L03L01-SRVSEL.sh
@@ -20,9 +20,13 @@ sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 #Create new directory /etc/nginx/html
 sudo ssh nginx mkdir --parents /etc/nginx/html
 
-#Copy example1.html and example2.html to /etc/nginx/html
+#Copy example1.html and example3.html to /etc/nginx/html
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/example{1,2,3,4}.html
-sudo scp /tmp/example{1,2,3,4}.html                          nginx:/etc/nginx/html
+sudo scp /tmp/example{1,3}.html                          nginx:/etc/nginx/html/
+
+#Copy example2.html and example4.html to /etc/nginx/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/example{1,2,3,4}.html
+sudo scp /tmp/example{2,4}.html                          nginx:/etc/nginx/
 
 #curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/mywebserver.bak
 #sudo scp /tmp/mywebserver.bak                              nginx:/etc/nginx/conf.d/

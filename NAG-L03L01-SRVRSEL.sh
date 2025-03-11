cabe4d19b60da6c5a034bfb291c88cc694e6ead7
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-03-11 12:29:44 -0700

    Update NAG-L03L01-SRVRSEL.sh
    added index.html for empty server to script

--- NAG-L03L01-SRVRSEL.sh
+++ NAG-L03L01-SRVRSEL.sh
@@ -20,6 +20,10 @@ sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
 #Create new directory /etc/nginx/html
 sudo ssh nginx mkdir --parents /etc/nginx/html
 
+#Copy the custom "for empty servers" index.html to /etc/nginx/html
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/index.html
+sudo scp /tmp/index.html                          nginx:/etc/nginx/html/
+
 #Copy example1.html and example3.html to /etc/nginx/html
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/example{1,2,3,4}.html
 sudo scp /tmp/example{1,3}.html                          nginx:/etc/nginx/html/

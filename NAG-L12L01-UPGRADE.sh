06af2eb42b9ef28d950c28817d0a19aa03a5a2b0
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-03-14 12:47:12 -0700

    Update NAG-L12L01-UPGRADE.sh

--- NAG-L12L01-UPGRADE.sh
+++ NAG-L12L01-UPGRADE.sh
@@ -14,7 +14,8 @@ set +x
 #echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-April-4-2025/nginx-one-eval.{crt,key,jwt}
 #NOT sure if need main directory in pathname since these files are not showing up in my lab system
 curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $LIC_TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/nginx/EXPIRES-April-4-2025/nginx-one-eval.{crt,key,jwt}
-echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/nginx/EXPIRES-April-4-2025/nginx-one-eval.{crt,key,jwt}set -x
+echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/nginx/EXPIRES-April-4-2025/nginx-one-eval.{crt,key,jwt}
+#set -x
 until sudo scp /tmp/nginx-one-eval.crt nginx:/etc/ssl/nginx/nginx-repo.crt || (( count++ > 5 )); do sleep 5; done
 until sudo scp /tmp/nginx-one-eval.key nginx:/etc/ssl/nginx/nginx-repo.key || (( count++ > 5 )); do sleep 5; done
 until sudo scp /tmp/nginx-one-eval.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done

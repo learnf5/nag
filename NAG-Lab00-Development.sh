35a60f4cf16b47ec0ca19a19cdbaf0ffb8218218
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-03-20 11:48:13 -0400

    Update NAG-Lab00-Development.sh

--- NAG-Lab00-Development.sh
+++ NAG-Lab00-Development.sh
@@ -7,8 +7,19 @@ PS4='+$(date +"%T.%3N"): '
 # curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab01/hosts
 # sudo cp /tmp/hosts                                  /etc/hosts
 
-# simple command to test and confirm this script is being run at launch.
-touch /home/student/Desktop/hello.txt
-
 # update nginx host for the specific lab
-# NOTHING specific for this lab - only the common lab files from common-tasks.sh
+
+#Remove default.conf file since this lab requires only default.bak
+sudo ssh nginx rm /etc/nginx/conf.d/default.conf
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/default.bak
+sudo scp /tmp/default.bak                              nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab07/mywebserver.conf
+sudo scp /tmp/mywebserver.conf                              nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab03/return_test.bak
+sudo scp /tmp/return_test.bak                              nginx:/etc/nginx/conf.d/
+
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/lab06/variable_test.bak
+sudo scp /tmp/variable_test.bak                              nginx:/etc/nginx/conf.d/
